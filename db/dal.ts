9a864bb7e1698bee62e24ffda433c8ac0f917b43
Gbemiga Atolagbe <gbatolagbe@gmail.com>
2024-10-12 00:51:36 +0100

    Remove NODE_ENV check

--- db/dal.ts
+++ db/dal.ts
@@ -1,30 +1,16 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { S3Client } from "@aws-sdk/client-s3";
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
-import { AppError } from "../lib/error";
+// import { AppError } from "../lib/error";
 
-let client: DynamoDBClient;
-
-if (process.env.NODE_ENV === "development") {
-  client = new DynamoDBClient({
-    region: "eu-north-1",
-    endpoint: "http://localhost:8000", // -- set only for local development
-    credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
-    },
-  });
-} else if (process.env.NODE_ENV === "production") {
-  client = new DynamoDBClient({
-    region: "eu-north-1",
-    credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
-    },
-  });
-} else {
-  throw new AppError("ENV not set", 500, "ENV not set", true); // not sure the error will be passed this way
-}
+const client = new DynamoDBClient({
+  region: "eu-north-1",
+  // endpoint: "http://localhost:8000", // -- set only for local development
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
+  },
+});
 
 export const dynamoDB = DynamoDBDocumentClient.from(client);
 
